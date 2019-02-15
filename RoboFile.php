1aa51b6d51d587f3f7dac7c2073a1fe87a9b1f1f
Will Long <will@kerasai.com>
2019-02-14 22:32:54 -0600

    initial commit

---
+++ RoboFile.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks {
+
+  use \Kerasai\Robo\Phpcs\loadTasks;
+
+  /**
+   * RoboFile constructor.
+   */
+  public function __construct() {
+    $this->stopOnFail();
+  }
+
+  /**
+   * Run tests.
+   */
+  public function test() {
+    $this->testPhpcs();
+    $this->testPhpunit();
+  }
+
+  /**
+   * PHPCS code style checks.
+   */
+  public function testPhpcs() {
+    $this->taskPhpcs()->run();
+  }
+  /**
+   * PHPUnit tests.
+   */
+  public function testPhpunit() {
+    $this->taskPhpUnit()->run();
+  }
+
+}
---
+++ src/ConnectionFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Kerasai\MySql;
+
+/**
+ * Class ConnectionFactory.
+ */
+class ConnectionFactory {
+
+  protected $config = [];
+  protected $connections = [];
+
+  /**
+   * ConnectionFactory constructor.
+   *
+   * @param array $config
+   *   Configuration array.
+   */
+  public function __construct(array $config = []) {
+    $this->config = $config;
+  }
+
+  /**
+   * Get a connection.
+   *
+   * @param string $name
+   *   The name of the connection.
+   *
+   * @throws \Exception
+   */
+  public function getConnection($name) {
+    if (empty($this->connections[$name])) {
+
+    }
+    return $this->connections[$name];
+  }
+
+  /**
+   * Gets configuration for the specified connection.
+   *
+   * @param string $name
+   *   The connection name.
+   *
+   * @return array
+   *   The connection configuration.
+   *
+   * @throws \Exception
+   */
+  public function getConfig($name) {
+    if (!array_key_exists($name, $this->config)) {
+      throw new \Exception(sprintf('No configuration available for connection "%s".', $name));
+    }
+    return $this->config[$name];
+  }
+
+  /**
+   * Set configuration for a connection.
+   *
+   * @param string $name
+   *   The connection name.
+   * @param array $config
+   *   The connection configuration.
+   *
+   * @return $this
+   */
+  public function setConfig($name, array $config) {
+    $this->config[$name] = $config;
+    return $this;
+  }
+
+}
